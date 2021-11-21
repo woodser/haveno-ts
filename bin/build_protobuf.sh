9c4a403e76b35a5a4d906c4a57479f3336f558b7
woodser <woodser@protonmail.com>
2021-11-21 10:29:06 -0500

    cleanup

--- bin/build_protobuf.sh
+++ bin/build_protobuf.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 # generate imports for haveno services and types using grpc-web
-
 mkdir -p ./src/protobuf
 cd ./src/protobuf || exit 1
-
 protoc -I=../../../haveno/proto/src/main/proto/ ../../../haveno/proto/src/main/proto/*.proto --js_out=import_style=commonjs,binary:./ --grpc-web_out=import_style=typescript,mode=grpcwebtext:./ || exit 1
