a02243d957e7994571f7174f9de9bdd846d7a674
napoly <napolytan@protonmail.com>
2022-12-08 09:19:14 +0100

    ignoring dist updating build script and envoy to latest

--- scripts/build_protobuf.sh
+++ scripts/build_protobuf.sh
@@ -12,8 +12,7 @@ protoc -I=./src_dir/ ./src_dir/*.proto --js_out=import_style=commonjs,binary:./ 
 tsc
 
 # copy protobuf models to dist
-mkdir -p dist/protobuf || exit 1
-cp ./grpc_pb.d.ts dist/protobuf/grpc_pb.d.ts || exit 1
-cp ./grpc_pb.js dist/protobuf/grpc_pb.js || exit 1
-cp ./pb_pb.d.ts dist/protobuf/pb_pb.d.ts || exit 1
-cp ./pb_pb.js dist/protobuf/pb_pb.js || exit 1
+cp ./grpc_pb.d.ts ../../dist/protobuf/grpc_pb.d.ts || exit 1
+cp ./grpc_pb.js ../../dist/protobuf/grpc_pb.js || exit 1
+cp ./pb_pb.d.ts ../../dist/protobuf/pb_pb.d.ts || exit 1
+cp ./pb_pb.js ../../dist/protobuf/pb_pb.js || exit 1
