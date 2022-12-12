4489bdee52894868af306fac61e9ebe633d5c8c3
woodser <woodser@protonmail.com>
2022-12-12 14:14:26 +0000

    support docker
    use latest envoy image

--- scripts/build_protobuf.sh
+++ scripts/build_protobuf.sh
@@ -3,4 +3,16 @@
 # generate imports for haveno services and types using grpc-web
 mkdir -p ./src/protobuf
 cd ./src/protobuf || exit 1
-protoc -I=../../../haveno/proto/src/main/proto/ ../../../haveno/proto/src/main/proto/*.proto --js_out=import_style=commonjs,binary:./ --grpc-web_out=import_style=typescript,mode=grpcwebtext:./ || exit 1
+mkdir -p ./src_dir
+wget https://raw.githubusercontent.com/haveno-dex/haveno/master/proto/src/main/proto/grpc.proto -O src_dir/grpc.proto -P ./src_dir
+wget https://raw.githubusercontent.com/haveno-dex/haveno/master/proto/src/main/proto/pb.proto -O src_dir/pb.proto -P ./src_dir
+protoc -I=./src_dir/ ./src_dir/*.proto --js_out=import_style=commonjs,binary:./ --grpc-web_out=import_style=typescript,mode=grpcwebtext:./ || exit 1
+
+# run typescript compiler
+tsc
+
+# copy protobuf models to dist
+cp ./grpc_pb.d.ts ../../dist/protobuf/grpc_pb.d.ts || exit 1
+cp ./grpc_pb.js ../../dist/protobuf/grpc_pb.js || exit 1
+cp ./pb_pb.d.ts ../../dist/protobuf/pb_pb.d.ts || exit 1
+cp ./pb_pb.js ../../dist/protobuf/pb_pb.js || exit 1
